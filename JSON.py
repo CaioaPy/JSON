60cd004a3ca994ed5cc181c775b4ae74e540f61c
yatomikazuchi <caioevilabner@gmail.com>
2024-04-22 20:45:13 -0300

    updated

--- JSON.py
+++ JSON.py
@@ -1,21 +1,12 @@
 import json
 
-lista = [
-    {
+#dict
+banco ={
     "name": "Francis",
     "ID": 59283122,
     "Work": "Unemployed",
     "Employed": False,
     "Likes": ["games", "Series"]
-    },
-    {
-    "name": "Marcus",
-    "ID": 1231262,
-    "Work": "Dev",
-    "Employed": True,
-    "Likes": ["learning", "teaching"]
     }
-]
 
-with open("UserData.json", "w") as f:
-    json.dump(lista, f)
+json.dump(banco)
